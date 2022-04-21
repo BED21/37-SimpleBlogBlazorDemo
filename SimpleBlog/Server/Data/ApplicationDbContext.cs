f16a7c7a5f14c3b394e3823c40a549673c7bf548
Claes Engelin <claes.engelin@zocom.se>
2022-04-21 13:39:10 +0200

    Add project files.

---
+++ SimpleBlog/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using SimpleBlog.Server.Models;
+
+namespace SimpleBlog.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
